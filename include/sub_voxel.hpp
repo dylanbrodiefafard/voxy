7391acd56a57d141d6255538cb510ac26e90c53c
Dylan Fafard (dyf477) <dylan.fafard@usask.ca>
2017-11-09 13:31:07 -0600

    Added some documentation and skeleton classes

---
+++ include/sub_voxel.hpp
@@ -0,0 +1,17 @@
+#ifndef VOXY_SUB_VOXEL_HPP
+#define VOXY_SUB_VOXEL_HPP
+
+
+namespace voxy
+{
+    class sub_voxel : public voxy
+    {
+        private:
+            voxel mVoxel;
+            // mesh
+            // color / texture
+
+    };
+}
+#endif // VOXY_SUB_VOXEL_HPP
+
